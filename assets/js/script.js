b7ed4faa9980e50903ddf82b0981c34fe174f2e0
nav380 <naveenchauhan380@gmail.com>
2026-01-02 19:33:48 +0530

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "backend development", "web designing", "android development", "web development"],
+    strings: ["Capital Markets Advisory","Investment Strategy","Private Equity & Debt Transactions","Real Estate Investment Services"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
